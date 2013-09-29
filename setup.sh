f1bb00ba7816391f050c5c966fb14cbfb019e232
Andreas Kristiansen <mail@ankr.dk>
2013-09-30 00:51:57 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,34 @@
 #/bin/bash
 
-current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+read -p "
+Hello my old friend!
+
+You probably don't even remember writing this script, but here goes...
+
+This will setup symoblic links for the config files.
+It will also DELETE any existing files!
+- are you sure you want to continue (y/n)? " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Where this script is located
+
+blacklist=('.' '..' 'setup.sh' 'README.md') # Do NOT link these files
 
 for file in $current/*
 do
-	base=$(basename $file)
+    base=$(basename $file)
 
-	if ! [ "$file" == '.' ] && ! [ "$file" == '..' ] && ! [ "$base" == 'setup.sh' ]; then
-		ln -s $file "$HOME/.$base"
-		echo "- Linked $base"
-	fi
+    if ! [ $(echo ${blacklist[@]} | grep $base | wc -l) -gt 0 ] ;
+    then
+        if [ -f "$HOME/.$base" ];
+        then
+            rm "$HOME/.$base" # Remove existing files
+        fi
+
+        ln -s $file "$HOME/.$base"
+        echo "Setup symbolic link for ~/.$base"
+    fi
 done
