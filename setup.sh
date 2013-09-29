fd704c58fccac7185cf29d03037a22eb80c17f9b
Andreas Kristiansen <mail@ankr.dk>
2013-09-30 01:43:31 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,4 @@
 #/bin/bash
-
-read -p "
-Hello my old friend!
-
-You probably don't even remember writing this script, but here goes...
-
-This will setup symoblic links for the config files.
-It will also DELETE any existing files!
-- are you sure you want to continue (y/n)? " -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
-
 current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Where this script is located
 
 blacklist=('.' '..' 'setup.sh' 'README.md') # Do NOT link these files
@@ -25,7 +11,8 @@ do
     then
         if [ -f "$HOME/.$base" ];
         then
-            rm "$HOME/.$base" # Remove existing files
+            mv "$HOME/.$base" "$HOME/.$base.bak" # Backup existing files
+            echo "Moved existing ~/.$base to $HOME/.$base.bak"
         fi
 
         ln -s $file "$HOME/.$base"
