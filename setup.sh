d27d6a6084d12d5c8bb8471aba0d5e0d9ad0a9aa
Kim Egede Jakobsen <kimegede@gmail.com>
2014-01-20 22:43:36 +0100

    Identation update

--- setup.sh
+++ setup.sh
@@ -1,22 +1,26 @@
 #/bin/bash
+
+#
+# Symlink files
+#
 for file in ~/.dotfiles/*
 do
-    base=$(basename $file)
+  base = $(basename $file)
 
-    # Only link certain files
-    if [ "$base" == "bash_profile" ] ||
-       [ "$base" == "bashrc" ] ||
-       [ "$base" == "gitconfig" ] ||
-       [ "$base" == "vimrc" ] ||
-       [ "$base" == "inputrc" ] ||
-       [ "$base" == "screenrc" ]
-    then
-        if [ -f "$HOME/.$base" ] ; then
-            mv "$HOME/.$base" "$HOME/.$base.bak" # Backup existing files
-            echo "Moved existing ~/.$base to ~/.$base.bak"
-        fi
+  # Only link certain files
+  if [ "$base" == "bash_profile" ] ||
+     [ "$base" == "bashrc" ] ||
+     [ "$base" == "gitconfig" ] ||
+     [ "$base" == "vimrc" ] ||
+     [ "$base" == "inputrc" ] ||
+     [ "$base" == "screenrc" ]
+  then
+      if [ -f "$HOME/.$base" ] ; then
+          mv "$HOME/.$base" "$HOME/.$base.bak" # Backup existing files
+          echo "Moved existing ~/.$base to ~/.$base.bak"
+      fi
 
-        ln -s $file "$HOME/.$base"
-        echo "Setup symbolic link for ~/.$base"
-    fi
+      ln -s $file "$HOME/.$base"
+      echo "Setup symbolic link for ~/.$base"
+  fi
 done
