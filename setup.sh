943ac822ad1909e8c9599d768eee3434f75c2c9c
Kim Egede Jakobsen <kj@epgs.com>
2014-01-22 19:55:25 +0100

    Check if OS is Mac before checking for Homebrew..

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #/bin/bash
+#
+# Check if OS is Mac
+#
+if [ "$(uname)" == "Darwin" ]; then
+  # Check if Homebrew is installed  
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  fi
 
-#
-# Check if Homebrew is installed
-#
-which -s brew
-if [[ $? != 0 ]] ; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  # Check if modules exists otherwise install it
+  modules=("bash-completion" "git" "git-extras" "tig" "screen" "vim" "pwgen" "htop" "tree" "wget")
+  for i in ${!modules[*]}
+  do
+    which -s ${modules[$i]} || brew install ${modules[$i]}
+  done
 fi
-
-#
-# Check if modules exists otherwise install it
-#
-modules=("bash-completion" "git" "git-extras" "tig" "screen" "vim" "pwgen" "htop" "tree" "wget")
-for i in ${!modules[*]}
-do
-  which -s ${modules[$i]} || brew install ${modules[$i]}
-done
 
 #
 # Symlink files
