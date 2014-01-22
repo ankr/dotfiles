4b4276a5021104417ce0450ee61e64fdd4c8a49a
Kim Egede Jakobsen <kj@epgs.com>
2014-01-22 20:00:56 +0100

    Ask user to install PHP 5.5
    If user answers "y" tab josegonzalez/homebrew-php and install depencies:
    * php55
    * php55-xdebug
    * php55-imagick
    * php55-mcrypt
    * mysql
    * composer

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Check if OS is Mac
 #
 if [ "$(uname)" == "Darwin" ]; then
-  # Check if Homebrew is installed  
+  # Check if Homebrew is installed
   which -s brew
   if [[ $? != 0 ]] ; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
@@ -15,6 +15,19 @@ if [ "$(uname)" == "Darwin" ]; then
   do
     which -s ${modules[$i]} || brew install ${modules[$i]}
   done
+
+  # Ask user to install PHP 5.5 + xdebug + imagick + mcrypt + mysql + composer
+  read -p "Install PHP 5.5 & related modules? [y/n]" response
+  if [[ $response == "y" ]]; then
+      brew tap homebrew/dupes
+      brew tap josegonzalez/homebrew-php
+      brew install php55
+      brew install php55-xdebug
+      brew install php55-imagick
+      brew install php55-mcrypt
+      brew install mysql
+      brew install josegonzalez/php/composer
+  fi
 fi
 
 #
@@ -22,7 +35,7 @@ fi
 #
 for file in ~/.dotfiles/*
 do
-  base = $(basename $file)
+  base=$(basename $file)
 
   # Only link certain files
   if [ "$base" == "bash_profile" ] ||
