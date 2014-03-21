31d1a7bb559253ec34a0c7ee51caaab69fae59df
Andreas Kristiansen <mail@ankr.dk>
2014-03-21 19:50:44 +0100

    Simplify install script

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ do
   base=$(basename $file)
   path="$HOME/.$base"
 
+
   # Backup existing files or delete if it's a symlink
-  if [ -f $path ] || [ -h $path ] ; then
-    if [ -h $path ] ; then
-      rm $path
-      echo "Removed existing symlink for $path"
-    else
-      mv $path "$path.bak"
-      echo "Moved existing $path to $path.bak"
-    fi
+  if [ -h $path ] ; then
+    rm $path
+    echo "Removed existing symlink for $path"
+  fi
+  if [ -f $path ] ; then
+    mv $path "$path.bak"
+    echo "Moved existing $path to $path.bak"
   fi
 
   ln -s $file $path
