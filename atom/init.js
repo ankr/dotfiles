c07295dfd45302317fdbbaff1106a065a7c1188d
Andreas Kristiansen <mail@ankr.dk>
2014-03-21 15:50:28 +0100

    Replace coffeescript style comments with javascript comments

--- atom/init.js
+++ atom/init.js
@@ -1,16 +1,16 @@
 // Your init script
-// # Atom will evaluate this file each time a new window is opened. It is run
-// # after packages are loaded/activated and after the previous editor state
-// # has been restored.
-// #
-// # An example hack to make opened Markdown files always be soft wrapped:
-// #
-// # path = require 'path'
-// #
-// # atom.workspaceView.eachEditorView (editorView) ->
-// #   editor = editorView.getEditor()
-// #   if path.extname(editor.getPath()) is '.md'
-// #     editor.setSoftWrap(true)
+// Atom will evaluate this file each time a new window is opened. It is run
+// after packages are loaded/activated and after the previous editor state
+// has been restored.
+//
+// An example hack to make opened Markdown files always be soft wrapped:
+//
+// path = require 'path'
+//
+// atom.workspaceView.eachEditorView (editorView) ->
+//   editor = editorView.getEditor()
+//   if path.extname(editor.getPath()) is '.md'
+//     editor.setSoftWrap(true)
 
 // Automatic fullscreen
 window.resizeTo(screen.availWidth, screen.availHeight);
